784d0a496a3e5e93c484e58f0f3e17766b79f30f
Kázmér Ádám Kerezsi <kazmer.kerezsi@stud.uni-corvinus.hu>
2021-11-17 00:12:33 +0100

    kovi

---
+++ week06/week06/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week06.Entities
+{
+    class RateData
+    {
+       public DateTime Date;
+       public string Currency;
+       public decimal Value;
+    }
+}
--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,17 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06.MnbServiceReference;
+using week06.Entities;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates;
+        string Results;
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+            chart1.DataSource = Rates;
+            atvaltas();
+            xml();
+            
+            
         }
-        private void atvaltas()
+        public void atvaltas()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -28,6 +38,34 @@ namespace week06
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+            Results = result;
+        }
+        public void xml()
+        {
+            
+            var xml = new XmlDocument();
+            xml.LoadXml(Results);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
+        }
+        public void chartolas()
+        {
+
         }
     }
 
